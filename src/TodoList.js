2f76ed9b9c5ccc6043334b46dd4b5cf4e88d94a8
Anton <antonbogatirev1@gmail.com>
2021-09-24 10:17:31 -0700

    Adding react-router
    Installed react-router-dom.
    Moved NewToDoForm into its own file.
    Added routes using react-router.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,19 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 import { connect } from "react-redux";
 import { addTodo, removeTodo } from "./actionCreators";
+import { Route } from "react-router-dom";
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.removeTodo = this.removeTodo.bind(this);
-        this.state = {
-            task: ""
-        };
+        this.handleAdd = this.handleAdd.bind(this);
     };
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.addTodo(this.state.task);
-        e.target.reset();
-    };
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    };
-    removeTodo(id){
+    handleAdd(val) {
+        this.props.addTodo(val);
+    }
+    removeTodo(id) {
         this.props.removeTodo(id);
     };
     render() {
@@ -36,12 +26,13 @@ class TodoList extends Component {
         );
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="task">Task</label>
-                    <input type="text" name="task" id="task" onChange={this.handleChange} />
-                    <button>Add a Todo!</button>
-                </form>
-                <ul>{todos}</ul>
+                <Route 
+                    path="/todos/new" 
+                    component={props => (
+                        <NewTodoForm {...props} handleSubmit={this.handleAdd} />
+                )}
+                />
+                <Route exact path="/todos" component={() => <div>{todos}</div>} />
             </div>
         );
     }
