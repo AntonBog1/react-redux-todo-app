0cf12acd2ee34c1ee51b9829f2d18430f75ff6ce
Anton <antonbogatirev1@gmail.com>
2021-09-26 12:23:08 -0700

    Connecting to back-end
    Installed redux-thunk.
    Added async functions to actionCreators.
    Refactoring rootReducer because Mongo is generating the Id's now.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>See Our Todos!</h1>
+        <h1>Welcome to the App!</h1>
         <p>
           <Link to="/todos">See my todos!</Link>
         </p>
